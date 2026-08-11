38dc23c0682d32de3ba4ce456d61009d46b9b54b
OvidioCalvet <calvetovidio@gmail.com>
2026-08-11 13:49:24 -0400

    feat: improve readFromFile func to print full lines using a buffer

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func readFromFile() {
@@ -11,16 +12,32 @@ func readFromFile() {
 		fmt.Println("Failed to open messages.txt")
 	}
 
+	buffer := ""
+
 	for {
+		// create an array of size 8 bytes
 		data := make([]byte, 8)
+		// read from file and populate 8 bytes from messages.txt
 		count, err := file.Read(data)
 		if err != nil {
-			break;
+			break //EOF
 		}
 
-		fmt.Printf("read: %s\n", string(data[:count]))
-	}
+		// convert bytes to string and add to buffer
+		chunk := string(data[:count])
+		buffer += chunk
 
+		// split buffer with every \n found
+		slices := strings.Split(buffer, "\n")
+
+		// if a \n was found print line and update buffer
+		if len(slices) > 1 {
+			for i := range len(slices) - 1 {
+				fmt.Printf("read: %s\n", slices[i])
+				buffer = slices[i + 1]
+			}
+		}
+	}
 }
 
 func main() {
