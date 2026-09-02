c185050476cb1e95325a61c75508ecef15738be7
OvidioCalvet <calvetovidio@gmail.com>
2026-09-02 15:50:09 -0400

    chore: save progress of Request line parsing implementation

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,3 +1,35 @@
 package request
 
-import ()
+import (
+	"io"
+	"log"
+	"strings"
+)
+
+type Request struct {
+    RequestLine RequestLine
+}
+
+type RequestLine struct {
+    HttpVersion   string
+    RequestTarget string
+    Method        string
+}
+
+func requestFromReader(reader io.Reader) (*Request, error) {
+	// read entire request as a single string
+	request, err := io.ReadAll(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func parseRequestLine(request string) (*string) {
+	requestLine := strings.Split(request, "\r\n")
+	if len(requestLine) > 0 {
+		return &requestLine[0]
+	}
+	err := "Parsing request line could not be done"
+	return &err
+}
